dc5957d1421edb86af8fb2daa201fa6d1d835445
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-08-30 12:14:19 -0500

    beginning title and URL retrieval

---
+++ rottenNameInteractive.py
@@ -0,0 +1,40 @@
+# Interactive Movie Name Query for pyRottenTomatoesScraper2k20
+# the purpose of this project is to provide an interactive interface by which
+# the user can conduct scraping based queries (no official or authorized)
+# APIs are used due to Rotten Tomatoes strict restrictions on researcher
+# use of their data, which undermines data scientists' access to 
+# information on the public reception of films
+
+# all pyRottenTomatoesScraper2k20 code is subject to GPL3, license in repo
+# copyright Eleanor A. Lockhart 2020
+# PURPOSE OF THIS PYTHON SCRIPT: provides an interactive query interface
+# by which the user can find out the ID/URL of a Rotten Tomatoes entry
+# for a particular film so that further scraping queries can be attempted
+
+# basic imports from libraries
+from lxml.html.soupparser import fromstring
+# root = fromstring(tag_soup)
+from bs4 import BeautifulSoup
+import requests
+
+# introductory script prints to console
+print("Interactive Movie Name Query Script")
+
+findMovie=1
+# first we need to find out what film the user wants to get the RT URL for
+while findMovie is 1:
+    print("Name of film to be queried:")
+    filmQueryName=input()
+    rtSearchURL="https://www.rottentomatoes.com/search?search="+filmQueryName
+    nowQueryBS4URL=rtSearchURL
+
+    # fetch raw HTML from Rotten Tomatoes search page for the film title
+    # being queried
+    html_content = requests.get(nowQueryBS4URL).text
+
+    # parse the RottenTomatoes results into readable form
+    soup = BeautifulSoup(features="lxml")
+    results = soup.find_all("a", {"class" : "info-name"})
+    for result in results :
+     if len(result.attrs) == 1 :
+        print(result)
